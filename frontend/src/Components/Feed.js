e0dbd8571e31e3ba008a70a2f96a80d6e2fa127e
Aeshna Verma <as289132@dal.ca>
2022-04-03 16:45:16 -0300

    add authentication for feed

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -4,20 +4,40 @@ import Footer from "./Footer/Footer";
 import { Grid, Paper, List, ListItem, ListItemAvatar, ListItemText, Divider, Typography } from "@material-ui/core";
 import Avatar from '@mui/material/Avatar';
 import { ReactionBarSelector } from '@charkour/react-reactions';
+import { AppContext } from "../context/userContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../common/constants";
 
 
 const Feed = () => {
 
-    const [userDetails, setUserDetails] = useState(
+  const { state: { authenticated, currentUser }, } = useContext(AppContext);
+  let navigate = useNavigate();
+  console.log("val auth ", authenticated)
+  if (!authenticated) {
+    navigate(ROUTES.HOMEPAGE);
+  }
+
+
+  useEffect(() => {
+    if (!authenticated) {
+     navigate(ROUTES.HOMEPAGE);
+   }
+
+
+
+},[]);
+
+
+const [userDetails, setUserDetails] = useState(
    
-        {"userName" : "Janet",
-         "url" : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-         "update" : "5pm",
-         "date" : "Apr - 2nd",
-        }
-    )
-
-
+  {"userName" : "Janet",
+   "url" : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+   "update" : "5pm",
+   "date" : "Apr - 2nd",
+  }
+)
 
 return (
 <>
@@ -25,9 +45,9 @@ return (
 
 <Grid>
 
-<Typography align="center"> My Feed </Typography>
+<Typography align="center" variant = "h4" style={{color:"#019267", padding: '20px'}}> Latest Updates </Typography>
     
-<Paper elevation={3} sx={{ width: '200px', height: '200px' }}>
+<Paper elevation={3} sx={{ width: '200px', height: '200px' }}> 
 
 <List>
 
