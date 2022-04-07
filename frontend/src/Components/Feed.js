4d662e44b744296b2852d75165b96a4fed938786
HanaPark <smilecana@gmail.com>
2022-04-07 18:29:34 -0300

    Added UserFeedStatus

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -1,18 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import NavigationBar from "./NavigationBar/Navbar";
-import Footer from "./Footer/Footer";
-import {Grid, Paper, List, ListItem, ListItemAvatar, ListItemText, Divider, Typography} from "@material-ui/core";
-import Avatar from '@mui/material/Avatar';
-import {ReactionBarSelector} from '@charkour/react-reactions';
+import {Typography} from "@material-ui/core";
 import {AppContext} from "../context/userContext";
-import {useContext} from "react";
 import {useNavigate} from "react-router-dom";
 import {ROUTES} from "../common/constants";
 import axios from "axios";
-import {Alert, CardActions, CardHeader, Snackbar, TextField} from "@mui/material";
+import {Alert, Snackbar} from "@mui/material";
 import Container from "@mui/material/Container";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import FeedCard from "./FeedCard";
 
 const Feed = () => {
 
@@ -35,33 +30,6 @@ const Feed = () => {
                     setFeedList(response.data.feeds);
             })
     }, []);
-
-
-    const [updates, setUpdates] = useState([
-
-            {
-                "userId": 1,
-                "userName": "Amber Reily",
-                "url": "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-                "text": "I finally took a step towards my social anxiety and signed up for a dance class!",
-                "date": "Apr - 2nd"
-            },
-            {
-                "userId": 2,
-                "userName": "John Bowie",
-                "url": "https://images.unsplash.com/photo-1590031905406-f18a426d772d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1244&q=80",
-                "text": "I joined a yoga class today! Looking forward to feeling better!",
-                "date": "Apr - 2nd"
-            },
-            {
-                "userId": 3,
-                "userName": "Sophia Zen",
-                "url": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                "text": "I started maintaining a journal! ",
-                "date": "Apr - 2nd"
-            }
-        ]
-    )
     const handleClick = (label, id) => {
         const data = {'badge' : label }
         axios.put(`/api/feed/${id}`,data,{headers: {
@@ -80,47 +48,12 @@ const Feed = () => {
         }
         setOpen(false);
     };
-    const buildNode = (emoji) => <div>{emoji}</div>;
-    const reactions = [{label: "congratsBadge", node: buildNode("🎊")},
-        {label: "fabBadge", node: buildNode("⭐")},
-        {label: "perfectBadge", node: buildNode("💯")},
-        {label: "awesomeBadge", node: buildNode("🙌")},
-        {label: "yaayBadge", node: buildNode("🤗")},
-        {label: "applauseBadge", node: buildNode("👏")},
-        {label: "celebrteBadge", node: buildNode("🎉")},
-        {label: "strongBadge", node: buildNode("💪")}
-    ]
     return (
         <>
             <NavigationBar/>
             <Container maxWidth="lg">
                 <Typography align="center" variant="h4" style={{color: "#019267", padding: '20px'}}> Latest Updates </Typography>
-                <Grid container spacing={3}>
-                    {feedList && feedList.map((feed, idx) => (
-                            <>
-                                <Grid item xs={4}>
-                                    <Card>
-                                        <CardHeader
-                                            avatar={
-                                                <Avatar sx={{width: '50px', height: '50px'}} src={"https://i.pravatar.cc/300?img=" + idx}> </Avatar>
-                                            }
-                                            title={feed.userName}
-                                            subheader={feed.createdAt}
-                                        />
-                                        <CardContent>
-                                            <Typography variant="body2">
-                                                {feed.feedText}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions sx={{paddingLeft:'9%'}}>
-                                            <ReactionBarSelector reactions={reactions} onSelect={e => handleClick(e, feed.feedId)} iconSize={'20px'}/>
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            </>
-                        )
-                    )}
-                </Grid>
+                <FeedCard onHandleClick = {handleClick} feedList = {feedList}/>
             </Container>
             <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
                 <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
--- frontend/src/Components/UserFeeds.js
+++ frontend/src/Components/UserFeeds.js
@@ -1,57 +1,98 @@
 import * as React from "react";
 import {useEffect, useState} from "react";
-import {ROUTES} from "../common/constants";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
-import {TextField} from "@mui/material";
-import Box from "@mui/material/Box";
-import {AccountCircle} from "@mui/icons-material";
-import {List, ListItem, ListItemAvatar, ListItemText} from "@material-ui/core";
+import {Alert, Badge, CardActions, CardHeader, Snackbar, TextField} from "@mui/material";
+import Card from "@mui/material/Card";
 import Avatar from "@mui/material/Avatar";
-import {ReactionBarSelector} from "@charkour/react-reactions";
+import CardContent from "@mui/material/CardContent";
+import {Typography} from "@material-ui/core";
 
-export default function UserFeeds(props) {
+const feed = [
+    {
+        "_id": "624e55cd163d5d57d54984eb",
+        "feedId": 1649300941853,
+        "userName": "Asmita",
+        "feedText": "I did yoga",
+        "congratsBadge": 1,
+        "fabBadge": 2,
+        "perfectBadge": 0,
+        "awesomeBadge": 0,
+        "yaayBadge": 0,
+        "applauseBadge": 0,
+        "celebrteBadge": 0,
+        "strongBadge": 0,
+        "createdAt": "2022-04-07T03:09:01.854Z",
+        "updatedAt": "2022-04-07T04:45:39.664Z",
+        "__v": 0
+    },
+    {
+        "_id": "624e6fce8b91a3ec6e5d8126",
+        "feedId": 1649307598706,
+        "userName": "Asmita",
+        "feedText": "I joined dance class",
+        "congratsBadge": 0,
+        "fabBadge": 0,
+        "perfectBadge": 0,
+        "awesomeBadge": 0,
+        "yaayBadge": 0,
+        "applauseBadge": 0,
+        "celebrteBadge": 0,
+        "strongBadge": 0,
+        "createdAt": "2022-04-07T04:59:58.713Z",
+        "updatedAt": "2022-04-07T04:59:58.713Z",
+        "__v": 0
+    }
+]
+export default function UserFeeds() {
     const [feedList, setFeedList] = useState([]);
     const [feedMsg, setFeedMsg] = useState('');
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState("")
     useEffect(() => {
-        getData();
-    },[1]);
+        setFeedList(feed);
+        // getData();
+    }, [1]);
     const getData = () => {
         // const data = {'username' : props.user.firstName}
-        const data = {'username' : 'Asmita'}
-        axios.get(`/api/feed/`, data, {headers: {
+        const data = {'username': 'Asmita'}
+        axios.get(`/api/feed/`, data, {
+            headers: {
                 'Content-Type': 'application/json'
-            }})
+            }
+        })
             .then((response) => {
-                if(response.data.success)
+                if (response.data.success)
                     setFeedList(response.data.experts);
             })
     }
+
     const handleChange = (e) => {
         setFeedMsg(e.target.value);
     }
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            const data = {'userName': 'Asmita', "feedText":feedMsg}
-            axios.post(`/api/feed/`,data,{headers: {
+            const data = {'userName': 'Asmita', "feedText": feedMsg}
+            axios.post(`/api/feed/`, data, {
+                headers: {
                     'Content-Type': 'application/json'
-                }})
+                }
+            })
                 .then((response) => {
-                    if(response.data.success)
-                        alert(response.data.message)
+                    if (response.data.success) {
+                        setOpen(true);
+                        setMsg(response.data.message)
+                    }
                 })
         }
     }
-    const buildNode = (emoji) => <div>{emoji}</div>;
-    const reactions = [{label: "congratsBadge", node: buildNode("🎊")},
-        {label: "fabBadge", node: buildNode("⭐")},
-        {label: "perfectBadge", node: buildNode("💯")},
-        {label: "awesomeBadge", node: buildNode("🙌")},
-        {label: "yaayBadge", node: buildNode("🤗")},
-        {label: "applauseBadge", node: buildNode("👏")},
-        {label: "celebrteBadge", node: buildNode("🎊")},
-        {label: "strongBadge", node: buildNode("💪")}
-    ]
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpen(false);
+    };
     return (
         <>
             <Grid container spacing={3} ml={2}>
@@ -59,32 +100,66 @@ export default function UserFeeds(props) {
                     <TextField
                         id="outlined-helperText"
                         label="Feed Message"
-                        value = {feedMsg}
+                        value={feedMsg}
                         fullWidth
                         onChange={handleChange}
                         onKeyDown={handleKeyDown}
                     />
                 </Grid>
-                <Grid>
-                    <List>
-                        {feedList && feedList.map((feed, idx) => (
-                                <>
-                                    <ListItem key={idx}>
-                                        <ListItemAvatar>
-                                            <Avatar sx={{width: '50px', height: '50px'}} src={"https://i.pravatar.cc/300?img=" + idx}> </Avatar>
-                                        </ListItemAvatar>
-                                        <ListItemText>
-                                            <ListItemText primary={feed.userName} secondary={feed.feedText}/>
-                                        </ListItemText>
-                                        <ReactionBarSelector reactions={reactions}>
-                                        </ReactionBarSelector>
-                                    </ListItem>
-                                </>
-                            )
-                        )}
-                    </List>
-                </Grid>
+                {feedList && feedList.map((feed, idx) => (
+                        <>
+                            <Grid item xs={4}>
+                                <Card>
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar sx={{width: '50px', height: '50px'}}
+                                                    src={"https://i.pravatar.cc/300?img=38"}> </Avatar>
+                                        }
+                                        title={feed.userName}
+                                        subheader={feed.createdAt}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="body2">
+                                            {feed.feedText}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Badge color="primary" badgeContent={feed.congratsBadge}>
+                                            🎊
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.fabBadge}>
+                                            ⭐
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.perfectBadge}>
+                                            💯
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.awesomeBadge}>
+                                            🙌
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.yaayBadge}>
+                                            🤗
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.applauseBadge}>
+                                            👏
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.celebrteBadge}>
+                                            🎉
+                                        </Badge>
+                                        <Badge color="primary" badgeContent={feed.strongBadge}>
+                                            💪
+                                        </Badge>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        </>
+                    )
+                )}
             </Grid>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{width: "100%"}}>
+                    {msg}
+                </Alert>
+            </Snackbar>
         </>
     );
 }
